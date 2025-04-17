c4b21008580f430c3926cd08bee22d6ba8104999
Ankush <ankushvashistha01@gmail.com>
2025-04-17 13:02:44 +0530

    ok

---
+++ loop.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for( int round = 1; round <= 5; round++){
+        cout<<"Round "<<endl;
+    }
+}
---
+++ odd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int A;
+    cin >> A;
+
+    if(A % 2 == 0) {
+        cout << "Even" <<endl;
+    } else {
+        cout << "Odd" <<endl;
+    }
+}
