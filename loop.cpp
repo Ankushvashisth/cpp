40cf93667eb802fbb9114d7b603b5bb33d027969
Ankush <ankushvashistha01@gmail.com>
2025-04-18 13:15:13 +0530

    ok

--- loop.cpp
+++ loop.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for( int round = 1; round <= 5; round++){
-        cout<<"Round "<<endl;
-    }
+    // for( int round = 1; round <= 5; round++){
+    //     cout<<"Round "<<endl;
+    // }
+    // for(int num = 4; num <= 100; num= num+4)
+    // cout<<num<<endl;
+//    int i = 4;
+//     while (i <= 100){
+//         cout<<i<<" ";
+//         i = i+4;
+
+//     }
+
+int sum = 0;
+for(int i = 1; i <= 100;  i = i+2){
+    sum = sum+i;
+}
+ cout<<sum;
 }
---
+++ prime.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+   for(int n = 1; n<=100; n++){
+    int a = 0;
+    if(n <1){
+        cout<<" n not a valid  no. : "<< n <<endl;
+    }else{
+for (int i = 2; i<=n/2; i++){
+    if(n % i == 0){
+        cout<<" n is not a prime : "<< n << endl;
+        a=1;
+        break;
+    } 
+}
+    
+if (a==0){
+    cout<<"n  is prime : "<< n <<endl;
+}
+}
+}
+}
